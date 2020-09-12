fbd8483f2e4cea63f45b08f7f090ac16b6dc3c6c
Phuc Duong <duongphuc203@gmail.com>
2020-09-12 10:00:02 +0700

    update: initialize project commit

---
+++ app/src/main/java/kotlin/phucduong/weather/MainActivity.kt
@@ -0,0 +1,19 @@
+package kotlin.phucduong.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.phucduong.weather.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/kotlin/phucduong/weather/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package kotlin.phucduong.weather.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
